ade138ca35e8cf2fb5304c6c98d7cbf393c83d54
xbkv <one.two.o.two@icloud.com>
2025-07-05 20:47:42 +0900

    Refactor __init__.py for readability

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,13 +1,36 @@
-from .zandaka import *
-from .rireki import *
-from .payout import *
-# from .payin import *
-# from .kouza import *
-from .send import *
-from .lottery import *
-# from .slot import *
-# from .roulette import *
-# from .gamble import *
-# from .blackjack import *
-from .mines import *
-from .account import *
+import discord
+
+from .balance import on_balance_command
+from .transfer import on_transfer_command
+from .payout import on_payout_command
+from .mines import on_mines_command
+from .flip import on_coinflip_command
+from .blackjack import on_blackjack_command
+from .dice import on_dice_command
+# from .rps import on_rps_command
+
+TEXT_COMMANDS = {
+    "$残高": on_balance_command,
+    "$送金": on_transfer_command,
+    "$出金": on_payout_command,
+    "$マインズ": on_mines_command,
+    "$フリップ": on_coinflip_command,
+    "$ダイス": on_dice_command,
+    "$bj": on_blackjack_command,
+    # "$じゃんけん": on_rps_command
+}
+
+async def register_all_text_commands(bot):
+    @bot.event
+    async def on_message(message: discord.Message):
+        if message.author.bot:
+            return
+
+        content = message.content.strip()
+
+        for cmd_prefix, handler in TEXT_COMMANDS.items():
+            if content.startswith(cmd_prefix):
+                await handler(message)
+                return
+
+        await bot.process_commands(message)
